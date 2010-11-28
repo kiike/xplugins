9b4dc1b550b7249da3ec99f815f4f6d18c5212e7
bgood@velocity.net <bgood@velocity.net@0ddca89e-5c49-5f5c-d725-acaa439a2e25>
2010-11-28 17:06:26 +0000

    trying to clean up my mess

---
+++ Xsaitekpanels/libusb_interface.h
@@ -0,0 +1,9 @@
+#ifndef LIBUSB_INTERFACE_H
+#define LIBUSB_INTERFACE_H
+
+
+//  Initilize libusb
+//  Returns true if no problems were encountered
+bool panels_init_libusb();
+
+#endif // LIBUSB_INTERFACE_H

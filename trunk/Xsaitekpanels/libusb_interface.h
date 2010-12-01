2418b508b015b003079ff2aae1522aad99af3cf6
bgood@velocity.net <bgood@velocity.net@0ddca89e-5c49-5f5c-d725-acaa439a2e25>
2010-12-01 04:47:35 +0000

    Getting close but am getting a build error

--- trunk/Xsaitekpanels/libusb_interface.h
+++ trunk/Xsaitekpanels/libusb_interface.h
@@ -1,15 +1,16 @@
 #ifndef LIBUSB_INTERFACE_H
 #define LIBUSB_INTERFACE_H
 
+#include <libusb-1.0/libusb.h>
+int radionumb;
 
 //  Initilize libusb get list get discriptors
 //  Find saitek radio multi and switch panels
 //  Returns true if no problems were encountered
 bool find_saitek_panels();
+//bool write_radio_panel();
+bool write_radio_panel(libusb_device_handle *radio_handle[], unsigned char radiowdata[4]);
 bool close_saitek_panels();
-
-
-
 
 
 #endif // LIBUSB_INTERFACE_H

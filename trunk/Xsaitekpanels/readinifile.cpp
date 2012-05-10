cf0b3dff1ce788ca96de2017f4809736501250e6
bgood@velocity.net <bgood@velocity.net@0ddca89e-5c49-5f5c-d725-acaa439a2e25>
2012-05-10 00:34:10 +0000

    Ver 1.40 have the Mac pathing completed but still needs tested.

--- trunk/Xsaitekpanels/readinifile.cpp
+++ trunk/Xsaitekpanels/readinifile.cpp
@@ -26,6 +26,7 @@
 string convert_Mac_Path(string in_path) {
 
     string out_path;
+    char seperator_number_buffer[255] = {0};
 
     XPLMDebugString("in_path = ");
     XPLMDebugString(in_path.c_str());
@@ -38,6 +39,14 @@ string convert_Mac_Path(string in_path) {
     XPLMDebugString("in_path = ");
     XPLMDebugString(in_path.c_str());
     XPLMDebugString("\n");
+
+    // Find how many path seperators in the path
+    int count = 0;
+    for (int i = 0; i < in_path.size(); i++)
+      if (in_path[i] == ':') count++;
+
+    sprintf(seperator_number_buffer, "How many path seperators are in the path = %d\n", count);
+    XPLMDebugString(seperator_number_buffer);
 
     size_t found;
     int n = 8;
